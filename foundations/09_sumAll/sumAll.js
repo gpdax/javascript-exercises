5eb2780020d2a54944787d12cdfee4d750af73d4
gpdax <mathpdalmeida3@gmail.com>
2026-03-10 18:19:57 -0300

    Solution: 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
 const sumAll = function() {
-
+    let args = Array.from(arguments);
+    let sum = 0;
+    if (args.some(arg => typeof arg !== 'number' || arg < 0)) {
+        return 'ERROR';
+    }
+    let min = Math.min(...args);
+    let max = Math.max(...args);
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll(1, 4)); // Output: 10
+
 
 // Do not edit below this line
 module.exports = sumAll;
