9a30cd28ed913921d6d23589d565c4fc60424948
gpdax <mathpdalmeida3@gmail.com>
2026-03-10 17:46:31 -0300

    Solution: 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
 const repeatString = function() {
+    let string = arguments[0];
+    let times = arguments[1];
+    let result = '';
+
+    if (times < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < times; i++) {
+        result += string;
+    }
+
+    return result;
 
 };
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
