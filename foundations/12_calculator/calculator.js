fb95e4be7f47342aab64db702ec41b85877a8b0d
gpdax <mathpdalmeida3@gmail.com>
2026-03-15 10:26:02 -0300

    Solution: 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a + b;
+  return sum;
+}
+
+const subtract = function(a,b) {
+	let difference = a - b;
+  return difference;
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	let power = a ** b;
+  return power;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+  
 };
 
 // Do not edit below this line
