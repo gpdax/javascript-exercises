085842dc3024776c5eac8646310c526439134b28
gpdax <mathpdalmeida3@gmail.com>
2026-03-29 19:35:48 -0300

    13_palindromes and 14_fibonacci resolutions

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+function palindromes(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    
+    // Check if the cleaned string is the same as its reverse
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+    
+palindromes("racecar")
+palindromes("rac3e3car")
+palindromes("A car, a man, a maraca.")
+palindromes("racecar!")
+palindromes("Racecar!")
+palindromes("ZZZZ car, a man, a maracaz.")
+palindromes("r3ace3car")
 
-};
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+function fibonacci(n) {
+    if (n <0){
+        return "OOPS";
+    }
+    else if (n === 0) {
+        return 0;
+    }
+    else if (n === 1) {
+        return 1;
+    }
+    else if (typeof n === "string") {
+        return fibonacci(Number(n));
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+   
 
-};
 
+
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 // Do not edit below this line
 module.exports = fibonacci;
