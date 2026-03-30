ea11ce7415a00041d4c47a7aaaa0eace59eb3c85
gpdax <mathpdalmeida3@gmail.com>
2026-03-30 18:58:08 -0300

    16_findTheOldest Solution

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'The Road Ahead',
+        titleor: 'Bill Gates',
+    },
+    {
+        title: 'Walter Isaacson',
+        titleor: 'Steve Jobs',
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        titleor: 'Suzanne Collins',
+    }
+];
 
-};
 
+function getTheTitles(books) {
+    return books.map(book => book.title);
+}
+
+getTheTitles(books);
+   
+        
 // Do not edit below this line
 module.exports = getTheTitles;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  if (!people.length) return undefined;
 
+  return people.reduce((oldest, person) => {
+    const currentYear = new Date().getFullYear();
+
+    const currentAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+    return currentAge > oldestAge ? person : oldest;
+  });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
